d042e8cbe054f25022be68ab0aa5df7ecccd911f
ClarkkCC <Clark113008@gmail.com>
2025-05-12 14:18:53 -0400

    Label and TItleScreen Code

---
+++ TitleScreen.java
@@ -0,0 +1,54 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Title Screen.
+ * 
+ * @author Clark
+ * @version May 2025
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Big Elephant", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject(titleLabel, 200, 100);
+        prepare();
+    }
+
+    /**
+     * The main world act loop
+     */
+    public void act()
+    {
+        // Start the game if user presses space bar
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld); 
+        }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,486,92);
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,285,360);
+        label.setLocation(315,351);
+        Label label2 = new Label("Use \u2190 and \u2192  to Move", 40);
+        addObject(label2,273,247);
+        label2.setLocation(265,219);
+        label.setLocation(303,305);
+    }
+}
